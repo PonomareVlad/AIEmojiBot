65decbe801bf7f39befba7b29a11e8292f672d24
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-08 07:32:50 +0500

    Fix

--- src/bot.mjs
+++ src/bot.mjs
@@ -14,21 +14,26 @@ const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
 const api = new API({token: OPENAI_API_KEY});
 
 bot.on("text", async ({reply, text, chat: {id}}) => {
-    const options = {
-        prompt: text,
-        max_tokens: 1000
-    };
-    await bot.sendAction(id, "typing");
-    const result = await api.completions(options);
-    if (result.trim().startsWith(`<svg`)) {
-        const options = {method: "post", body: result};
-        const response = await fetch(`https://${VERCEL_URL}/api/send?id=${id}`, options);
-        const json = await response.json();
-        console.debug(json);
-        const message = md.build(`Send result: ${md.codeBlock(JSON.stringify(json), "json")}`);
-        return reply.text(message, {parseMode: "MarkdownV2"});
+    try {
+        const options = {
+            prompt: text,
+            max_tokens: 1000
+        };
+        await bot.sendAction(id, "typing");
+        const result = await api.completions(options);
+        if (result.trim().startsWith(`<svg`)) {
+            const options = {method: "post", body: result};
+            const response = await fetch(`https://${VERCEL_URL}/api/send?id=${id}`, options);
+            const json = await response.json();
+            console.debug(json);
+            const message = md.build(`Send result: ${md.codeBlock(JSON.stringify(json), "json")}`);
+            return reply.text(message, {parseMode: "MarkdownV2"});
+        }
+        return reply.text(md.build(result), {parseMode: "MarkdownV2"});
+    } catch (e) {
+        console.error(e);
+        return reply.text(e.message);
     }
-    return reply.text(md.build(result), {parseMode: "MarkdownV2"});
 });
 
 bot.plug(shortReply);
