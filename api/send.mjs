f5f7a6ae13c3747c51f0c68f028d9e29596c8170
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-08 07:30:33 +0500

    Fix

--- api/send.mjs
+++ api/send.mjs
@@ -1,6 +1,6 @@
 import {optimize} from "svgo";
-import bot from "../src/bot.mjs"
 import {md} from "telegram-md";
+import bot from "../src/bot.mjs";
 import {serializeError} from "serialize-error";
 import {convert, options} from "../src/svg.mjs";
 
@@ -9,8 +9,8 @@ export default async ({query: {id}, body}, {json}) => {
         if (!id || !body) return json({status: false});
         await bot.sendAction(id, "upload_document");
         const {data} = optimize(body, options);
-        const message = md.build(`Optimized SVG: ${md.codeBlock(data, "svg")}`);
-        await bot.sendMessage(id, message, {parseMode: "MarkdownV2"}).catch(e => e);
+        // const message = md.build(`Optimized SVG: ${md.codeBlock(data, "svg")}`);
+        // await bot.sendMessage(id, message, {parseMode: "MarkdownV2"}).catch(e => e);
         const sticker = await convert(data);
         await bot.sendAction(id, "choose_sticker");
         return json(await bot.sendDocument(id, sticker, {fileName: "sticker.tgs"}).catch(e => e));
