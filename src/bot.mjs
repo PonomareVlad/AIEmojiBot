8bc26e840a7e856c41e81bb4a9b1db0ab3b9c074
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-13 11:19:54 +0500

    Fixes

--- src/bot.mjs
+++ src/bot.mjs
@@ -57,7 +57,7 @@ class AIEmojiBot extends NewMethodsMixin(TeleBot) {
             } = user.messages;
             const max_tokens = maxTokens - tokens;
             console.debug("Messages:", {length, tokens});
-            if (max_tokens < 1000) return reply.text(strings.limit);
+            if (max_tokens < 1500) return reply.text(strings.limit);
             const result = await api.chat({max_tokens, messages: history});
             const structure = marked.lexer(result, {});
             const messages = structure.reduce((messages = [[]], {type, text, raw, lang, tokens} = {}) => {
